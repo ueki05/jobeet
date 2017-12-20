74c914be0a4ddb13939bd0fce6a44de36d136aaa
ueki05 <uekiyuta1991@gmail.com>
2017-12-21 01:21:45 +0900

    インデント修正

--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -7,16 +7,16 @@
  */
 class JobeetJobTable extends Doctrine_Table
 {
-    /**
-     * Returns an instance of this class.
-     *
-     * @return object JobeetJobTable
-     */
-    public static function getInstance()
-    {
-        return Doctrine_Core::getTable('JobeetJob');
-    }
-  // ↑ 別コミットでインデント修正します
+  /**
+   * Returns an instance of this class.
+   *
+   * @return object JobeetJobTable
+   */
+  public static function getInstance()
+  {
+    return Doctrine_Core::getTable('JobeetJob');
+  }
+
   public function getActiveJobs()
   {
     $q = $this->createQuery('j')
