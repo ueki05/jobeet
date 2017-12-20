03feb090be58d38afa65e487aaaa76eef0de145f
ueki05 <uekiyuta1991@gmail.com>
2017-12-21 01:47:14 +0900

    ホームページでのカテゴリ表示

--- lib/model/doctrine/JobeetJob.class.php
+++ lib/model/doctrine/JobeetJob.class.php
@@ -23,6 +23,11 @@ class JobeetJob extends BaseJobeetJob
     return parent::save($conn);
   }
 
+  public function __toString()
+  {
+    return sprintf('%s at %s (%s)', $this->getPosition(), $this->getCompany(), $this->getLocation());
+  }
+
   public function getCompanySlug()
   {
     return Jobeet::slugify($this->getCompany());
