f77cf83f7ed449cfbf0f2ca40d840e1a91928cb7
ueki05 <uekiyuta1991@gmail.com>
2017-12-24 02:33:11 +0900

    データベースを更新する

--- lib/filter/doctrine/base/BaseJobeetCategoryFormFilter.class.php
+++ lib/filter/doctrine/base/BaseJobeetCategoryFormFilter.class.php
@@ -16,6 +16,7 @@ abstract class BaseJobeetCategoryFormFilter extends BaseFormFilterDoctrine
       'name'                   => new sfWidgetFormFilterInput(array('with_empty' => false)),
       'created_at'             => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
       'updated_at'             => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+      'slug'                   => new sfWidgetFormFilterInput(),
       'jobeet_affiliates_list' => new sfWidgetFormDoctrineChoice(array('multiple' => true, 'model' => 'JobeetAffiliate')),
     ));
 
@@ -23,6 +24,7 @@ abstract class BaseJobeetCategoryFormFilter extends BaseFormFilterDoctrine
       'name'                   => new sfValidatorPass(array('required' => false)),
       'created_at'             => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
       'updated_at'             => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+      'slug'                   => new sfValidatorPass(array('required' => false)),
       'jobeet_affiliates_list' => new sfValidatorDoctrineChoice(array('multiple' => true, 'model' => 'JobeetAffiliate', 'required' => false)),
     ));
 
@@ -35,11 +37,7 @@ abstract class BaseJobeetCategoryFormFilter extends BaseFormFilterDoctrine
     parent::setup();
   }
 
-  public function add
-Deprecated: preg_replace(): The /e modifier is deprecated, use preg_replace_callback instead in /Applications/MAMP/htdocs/jobeet/lib/vendor/symfony/symfony-1.4.20/lib/util/sfToolkit.class.php on line 362
-
-Deprecated: preg_replace(): The /e modifier is deprecated, use preg_replace_callback instead in /Applications/MAMP/htdocs/jobeet/lib/vendor/symfony/symfony-1.4.20/lib/util/sfToolkit.class.php on line 362
-JobeetAffiliatesListColumnQuery(Doctrine_Query $query, $field, $values)
+  public function addJobeetAffiliatesListColumnQuery(Doctrine_Query $query, $field, $values)
   {
     if (!is_array($values))
     {
@@ -69,6 +67,7 @@ JobeetAffiliatesListColumnQuery(Doctrine_Query $query, $field, $values)
       'name'                   => 'Text',
       'created_at'             => 'Date',
       'updated_at'             => 'Date',
+      'slug'                   => 'Text',
       'jobeet_affiliates_list' => 'ManyKey',
     );
   }
