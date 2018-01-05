2ba2b8e0136ca669b3ed5ac0eaee27e94b3fce19
ueki05 <uekiyuta1991@gmail.com>
2018-01-06 04:04:04 +0900

    機能テストを書く

--- lib/test/JobeetTestFunctional.class.php
+++ lib/test/JobeetTestFunctional.class.php
@@ -1,6 +1,18 @@
 <?php
 class JobeetTestFunctional extends sfTestFunctional
 {
+  public function getMostRecentProgrammingJob()
+  {
+    $q = Doctrine_Query::create()
+      ->select('j.*')
+      ->from('JobeetJob j')
+      ->leftJoin('j.JobeetCategory c')
+      ->where('c.slug = ?', 'programming');
+    $q = Doctrine_Core::getTable('JobeetJob')->addActiveJobsQuery($q);
+
+    return $q->fetchOne();
+  }
+
   public function loadData()
   {
     Doctrine_Core::loadData(sfConfig::get('sf_test_dir').'/fixtures');
