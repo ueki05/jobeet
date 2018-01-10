b0134c79052315d0a75a04d0e14d0a2b44d943fb
ueki05 <uekiyuta1991@gmail.com>
2018-01-11 02:39:26 +0900

    テストで未来に戻る

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -86,4 +86,16 @@ class jobActions extends sfActions
 
     $this->redirect('job_show_user', $job);
   }
+
+  public function executeExtend(sfWebRequest $request)
+  {
+    $request->checkCSRFProtection();
+
+    $job = $this->getRoute()->getObject();
+    $this->forward404Unless($job->extend());
+
+    $this->getUser()->setFlash('notice', sprintf('Your job validity has been extended until %s.', $job->getDateTimeObject('expires_at')->format('m/d/Y')));
+
+    $this->redirect('job_show_user', $job);
+  }
 }
